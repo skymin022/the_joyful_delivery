e38820f50dac14119c6a17205f2901ce9d87833f
Lee-0210 <tjsans9069@gmail.com>
2025-05-22 11:38:39 +0900

    admin 회원목록 페이지 구현

--- the_joyful_delivery/src/main/java/service/UserService.java
+++ the_joyful_delivery/src/main/java/service/UserService.java
@@ -1,6 +1,8 @@
 package service;
 
 import java.util.List;
+import java.util.Map;
+
 import com.alohaclass.jdbc.dto.PageInfo;
 import DTO.Users;
 
@@ -8,6 +10,7 @@ public interface UserService {
 
 	// C.R.U.D
 	public List<Users> list();
+	public List<Users> listBy(Map<String, Object> fields);
 	public PageInfo<Users> page();
 	public Users select(int no);
 	public int insert(Users user);
