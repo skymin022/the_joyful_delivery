41e106fbee82c126d0afa0ea53831fea1fac15ed
Lee-0210 <tjsans9069@gmail.com>
2025-09-04 17:08:40 +0900

    문제였던 로직 수정

--- the_joyful_delivery/src/main/java/servlet/PayServlet.java
+++ the_joyful_delivery/src/main/java/servlet/PayServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import DTO.Delivery;
 import DTO.Payment;
+import DTO.RegionName;
 import DTO.SAR;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,6 +16,8 @@ import service.DeliveryService;
 import service.DeliveryServiceImpl;
 import service.PayService;
 import service.PayServiceImpl;
+import service.RegionService;
+import service.RegionServiceImpl;
 import service.SARService;
 import service.SARServiceImpl;
 
@@ -24,6 +27,7 @@ public class PayServlet extends HttpServlet {
 	DeliveryService delService = new DeliveryServiceImpl();
 	SARService sarService = new SARServiceImpl();
 	PayService payService = new PayServiceImpl();
+	RegionService regionService = new RegionServiceImpl();
 	
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -51,7 +55,8 @@ public class PayServlet extends HttpServlet {
     			if(result2 != 0) {
     				Payment payment = Payment.builder().dIdx(delivery.getIdx()).pAmount(price).pCard(cardNumber).build();
     				payService.insert(payment);
-    				
+    				RegionName region = RegionName.builder().delIdx(delivery.getIdx()).status("인천 부평구").build();
+    				regionService.insert(region);
     				System.out.println("배송정보 등록 완료.");
     				response.sendRedirect(root + "/paysuccess.jsp");
     			} 
